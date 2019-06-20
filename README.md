c4a9130b787f37a68f79e41518f080939ffb62d8
Alberto Acerbi <alberto.acerbi@gmail.com>
2019-06-20 12:00:46 +0100

    analysis done

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 # vaccines-wellcome
 
-The original data are:
+The original data are from:
 
-* Wellcome trust (vaccine hesitancy, trust in science, religious attitudes - directly downaload an Excel file): https://wellcome.ac.uk/sites/default/files/wgm2018-dataset-crosstabs-all-countries.xlsx
+* Wellcome trust (vaccine hesitancy, trust in science, importance of religion - directly downaload an Excel file): https://wellcome.ac.uk/sites/default/files/wgm2018-dataset-crosstabs-all-countries.xlsx
 
-* International Monetary Found (GDP pro capita): https://www.imf.org/external/pubs/ft/weo/2018/02/weodata/
+* International Monetary Found (GDP pro capita): https://www.imf.org/external/pubs/ft/weo/2019/01/weodata/
 
-* Eurostat (social media usage): https://ec.europa.eu/eurostat/web/digital-economy-and-society/data/main-tables
+* Eurostat (social media penetration): https://ec.europa.eu/eurostat/web/digital-economy-and-society/data/main-tables
+
+The data used for the analysis are extracted in the folder with the same name. 
+
+Notice that I used the following data:
+
+* **Vaccine heistancy**: Percentage of people who answered 'Strongly or somewhat disagree that vaccines are safe' to the question 'Do you agree, disagree, or neither agree nor disagree with the following statements? Vaccines are effective. Vaccines are safe' (Wellcome Global Monitor 2018)
+
+* **Trust in science**: Wellcome Global Monitor Trust in Scientists Index, Country Average Index Score (Wellcome Global Monitor 2018)
+
+* **Importance of religion**: Percentage of people who have a religion & say it is important in their daily lives (Wellcome Global Monitor 2018)
+
+* **GDP pro capita**: estimates in International Dollars from the International Monetary Found
+
+* **Social media penetration**: category 'Internet use: participating in social networks (creating user profile, posting messages or other contributions to facebook, twitter, etc.)', percentage on total individuals in 2018 (Eurostat - Digital economy and society) 
+
+The R script [correlations.R](correlations.R) reproduce the plots (that are also in the folder with the same name) 
--- correlations.R
+++ correlations.R
@@ -1,41 +1,119 @@
-library( tidyverse)
+library(tidyverse)
 
-europeanUnion <- c("Austria","Belgium","Bulgaria","Croatia","Cyprus",
-                   "Czech Rep.","Denmark","Estonia","Finland","France",
-                   "Germany","Greece","Hungary","Ireland","Italy","Latvia",
-                   "Lithuania","Luxembourg","Malta","Netherlands","Poland",
-                   "Portugal","Romania","Slovakia","Slovenia","Spain",
-                   "Sweden","United Kingdom")
+# load data:
 
-social_media <- c(51,72,50,47,63,48,75,60,66,43,51,50,65,59,43,60,54,68,70,67,48,56,
-                  52,59,45,57,71,71)
+social_media <- read_csv(file = 'data/eurostat_internet_use.csv')
 
-measles_trend <- rep(0,28)
+hesitancy <- read_csv(file = "data/wellcome_safe_effective_vaccines.csv", 
+                      locale = locale(encoding = "latin1"), col_names = FALSE)
 
-# now for vaccinations:
-measles <- read_csv("API_SH.IMM.MEAS_DS2_en_csv_v2_10034419.csv") %>%
-  select( `Country Name`, `1960`:`2016`) %>%
-  rename( Country = `Country Name` ) %>%
-  gather( Year, Percentage, -Country) %>%
-  arrange( Country )
-measles$Percentage <- as.double(measles$Percentage)
-measles$Year <- as.integer(measles$Year)
+religion <- read_csv(file = "data/wellcome_religion.csv", 
+                      locale = locale(encoding = "latin1"), col_names = FALSE)
 
-europeanUnion[6]="Czech Republic"
-europeanUnion[24]="Slovak Republic"
-counter = 1
-for( i in europeanUnion ){
-  measles_trend[counter] <- measles$Percentage[measles$Country==i & measles$Year==2016] -
-    measles$Percentage[measles$Country==i & measles$Year==2010]
-  counter = counter + 1 
-}
+science <- read_csv(file = 'data/wellcome_trust_science.csv',
+                      locale = locale(encoding = "latin1"), col_names = FALSE)
+
+gdp <- read_csv(file = 'data/imo_gdp_capita.csv')
+
+# clean data:
+
+# social media:
+social_media <- social_media %>%
+  filter(INDIC_IS == 'Internet use: participating in social networks (creating user profile, posting messages or other contributions to facebook, twitter, etc.)') %>%
+  filter(TIME == '2018') %>%
+  filter(UNIT == 'Percentage of individuals') %>%
+  filter(Value != ":") %>%
+  mutate(Value = as.integer(Value)/100) %>%
+  rename('Country' = 'GEO')
+
+social_media$Country[40] <- 'Kosovo'
+social_media$Country[9] <- 'Germany'
+social_media$Country[7] <- 'Czech Republic'
+
+# hesitancy:
+hesitancy <- hesitancy %>%
+  rename('Country' = 'X2') %>%
+  rename('Proportion' = 'X8') %>%
+  mutate(Proportion = str_remove( Proportion, '%')) %>%
+  mutate(Proportion = as.integer(Proportion) / 100) 
+  
+hesitancy$Country[77] <- 'North Macedonia'
+hesitancy$Country[136] <- 'United Kingdom'
+
+# religion:
+religion <- religion %>%
+  rename('Country' = 'X2') %>%
+  rename('Proportion' = 'X8') %>%
+  mutate(Proportion = str_remove( Proportion, '%')) %>%
+  mutate(Proportion = as.integer(Proportion) / 100)  
+
+religion$Country[71] <- 'North Macedonia'
+religion$Country[125] <- 'United Kingdom'
+
+# science:
+science <- science %>%
+  rename('Country' = 'X2') %>%
+  rename('TSI' = 'X8') 
+
+science$Country[77] <- 'North Macedonia'
+science$Country[135] <- 'United Kingdom'
+
+gdp <- gdp %>%
+  rename('GDP' = '2018') %>%
+  mutate(GDP = str_remove(GDP, '\\...')) %>%
+  mutate(GDP = str_remove(GDP, ',')) %>%
+  mutate(GDP = as.double(GDP))
 
 
-out = data_frame( social_media, measles_trend)
-ggplot(data=out, aes(x=measles_trend, y =social_media)) +
-  geom_point()+
-  geom_smooth(method = lm)+
-  scale_y_continuous(name='Social media usage (%)')+
-  scale_x_continuous(name='2010/2016 trend for measles vaccination (%)')+
-  theme_bw()+
-  ggsave('correlation.pdf',width = 4, height = 4)
+# process data for correlation analysis:
+# we use only countries that are in the social_media dataset:
+
+EU_data <- inner_join(social_media, hesitancy, by = 'Country')  %>%
+  select(Country, Value, Proportion) %>%
+  rename('Social_media' = 'Value') %>%
+  rename('Hesitancy' = 'Proportion') %>%
+  inner_join(religion, by = 'Country') %>%
+  select( Country, Social_media, Hesitancy, Proportion) %>%
+  rename('Religion' = 'Proportion') %>%
+  inner_join(science, by = 'Country') %>%
+  select( Country, Social_media, Hesitancy, Religion, TSI) %>%
+  inner_join(gdp, by = 'Country') %>%
+  select( Country, Social_media, Hesitancy, Religion, TSI, GDP) 
+
+# plot
+
+basic_plot <- ggplot(data = EU_data, aes(x = Hesitancy, label = Country)) +
+  theme_bw() +
+  labs(x = 'Vaccine hesitancy') 
+
+# social media:
+basic_plot + 
+  geom_smooth(aes(y = Social_media), method = lm) +
+  geom_point(aes(y = Social_media), col = "red") +
+  geom_text_repel(aes(y = Social_media)) +
+  labs(y = 'Social media penetration') +
+  ggsave('plots/Social_media.pdf',width = 8, height = 6)
+
+# religion:
+basic_plot + 
+  geom_smooth(aes(y = Religion), method = lm) +
+  geom_point(aes(y = Religion), col = "red") +
+  geom_text_repel(aes(y = Religion)) +
+  labs(y = 'Importance of religion') +
+  ggsave('plots/Religion.pdf',width = 8, height = 6)
+
+# science:
+basic_plot + 
+  geom_smooth(aes(y = TSI), method = lm) +
+  geom_point(aes(y = TSI), col = "red") +
+  geom_text_repel(aes(y = TSI)) +
+  labs(y = 'Trust in science') +
+  ggsave('plots/Science.pdf',width = 8, height = 6)
+
+# GDP:
+basic_plot + 
+  geom_smooth(aes(y = GDP), method = lm) +
+  geom_point(aes(y = GDP), col = "red") +
+  geom_text_repel(aes(y = GDP)) +
+  labs(y = 'GPD pro capita') +
+  ggsave('plots/GDP.pdf',width = 8, height = 6)
